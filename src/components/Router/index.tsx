5fc8d4ed2d94dbfadf214476b9b12993650c4a60
evergreen <dany2zverev@gmail.com>
2022-03-30 17:53:02 +0300

    V-07 refactoring for gh-pages, add gh-pages branch

--- src/components/Router/index.tsx
+++ src/components/Router/index.tsx
@@ -1,18 +1,37 @@
-import React, { ReactNode } from 'react';
-import { HashRouter as BaseRouter } from 'react-router-dom';
+import React, { ReactNode, useLayoutEffect, useState } from 'react';
+import { History } from 'history';
+import { Router as BaseRouter } from 'react-router-dom';
 
 interface RouterProps {
   basename?: string;
   children: ReactNode;
+  history: History;
 }
 
-export const Router = ({ basename, children }: RouterProps): React.ReactElement => (
-  <BaseRouter
-    basename={basename}
-  >
-    {children}
-  </BaseRouter>
-);
+// eslint-disable-next-line import/prefer-default-export
+export const Router = ({
+  basename,
+  children,
+  history,
+}: RouterProps): React.ReactElement => {
+  const [state, setState] = useState({
+    action: history.action,
+    location: history.location,
+  });
+
+  useLayoutEffect(() => history.listen(setState), [history]);
+
+  return (
+    <BaseRouter
+      basename={basename}
+      location={state.location}
+      navigationType={state.action}
+      navigator={history}
+    >
+      {children}
+    </BaseRouter>
+  );
+};
 
 Router.defaultProps = {
   basename: '/',
--- src/features/Image/hooks.ts
+++ src/features/Image/hooks.ts
@@ -1,8 +1,8 @@
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchImage } from './thunks';
-import { ImageState } from '../../../../untitled/src/types/Image';
-import { Dispatcher } from '../../../../untitled/src/types/store';
+import { ImageState } from '../../types/Image';
+import { Dispatcher } from '../../types/store';
 
 // eslint-disable-next-line import/prefer-default-export
 export const useImage = (): [boolean, boolean, string] => {
