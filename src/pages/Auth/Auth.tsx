ba72ac200d387adc4ca3df6fb0ddcc95d2a7e742
evergreen <dany2zverev@gmail.com>
2022-03-30 16:30:13 +0300

    V-07 add Auth page

--- src/pages/Auth/Auth.tsx
+++ src/pages/Auth/Auth.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
+import { Button, Form } from 'react-bootstrap';
 import Logo from '../../assets/logo.component.svg';
+import styles from './styles.module.scss';
 
 const Auth = (): React.ReactElement => (
   <>
@@ -9,8 +11,38 @@ const Auth = (): React.ReactElement => (
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="description" content="Home page" />
     </Helmet>
-    <div>
+    <div className={styles['auth-wrap']}>
       <Logo />
+      <div className={styles['auth']}>
+        <h1>
+          Вход
+        </h1>
+        <Form className={styles['auth__form']}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Почта</Form.Label>
+            <Form.Control type="email" placeholder="Введите почту" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Пароль</Form.Label>
+            <Form.Control type="password" placeholder="Введите пароль" />
+          </Form.Group>
+          <Form.Group className={styles['btn-wrap']}>
+            <button
+              type="button"
+              className={styles['request-access-btn']}
+            >
+              Запросить доступ
+            </button>
+            <Button
+              variant="primary"
+              type="submit"
+            >
+              Войти
+            </Button>
+          </Form.Group>
+        </Form>
+      </div>
     </div>
   </>
 );
