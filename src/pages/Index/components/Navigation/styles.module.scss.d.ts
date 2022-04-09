065e18288595e203e8d33b6fd3e21e0e4e81bd79
evergreen <dany2zverev@gmail.com>
2022-04-09 14:07:45 +0300

    V-08 add Navigation Menu

---
+++ src/pages/Index/components/Navigation/styles.module.scss.d.ts
@@ -0,0 +1,16 @@
+declare namespace StylesModuleScssNamespace {
+  export interface IStylesModuleScss {
+    "nav-item": string;
+    "nav-item-selected": string;
+    navigation: string;
+    navigation__logo: string;
+    navigation__menu: string;
+  }
+}
+
+declare const StylesModuleScssModule: StylesModuleScssNamespace.IStylesModuleScss & {
+  /** WARNING: Only available when `css-loader` is used without `style-loader` or `mini-css-extract-plugin` */
+  locals: StylesModuleScssNamespace.IStylesModuleScss;
+};
+
+export = StylesModuleScssModule;
