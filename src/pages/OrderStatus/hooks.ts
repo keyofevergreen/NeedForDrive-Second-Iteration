46002aaee3da7db4451c61ac809e22302ea87985
evergreen <dany2zverev@gmail.com>
2022-04-22 17:53:56 +0300

    F-09 refactoring

---
+++ src/pages/OrderStatus/hooks.ts
@@ -0,0 +1,30 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchOrderStatus } from '../../store/OrderStatus/thunks';
+import { Dispatcher } from '../../types/store';
+import { OrderStatus, OrderStatusState } from '../../types/OrderStatus';
+import { SearchState } from '../../types/Search';
+
+export const useOrderStatus = (): [OrderStatus[], boolean, string | null] => {
+  const dispatch = useDispatch<Dispatcher>();
+  const { orderStatus, loading, error } = useSelector<{
+    orderStatus: OrderStatusState;
+  }, OrderStatusState>((state) => state.orderStatus);
+
+  useEffect(() => {
+    dispatch(fetchOrderStatus());
+  }, []);
+
+  return [orderStatus, loading, error];
+};
+
+export const useSortedOrderStatus = (orderStatuses: OrderStatus[]): OrderStatus[] => {
+  const { searchSort } = useSelector<{
+    search: SearchState;
+  }, SearchState>((state) => state.search);
+
+  if (orderStatuses) {
+    return orderStatuses.filter((orderStatus) => orderStatus.name.includes(searchSort));
+  }
+  return [];
+};
---
+++ src/pages/Points/hooks.ts
@@ -0,0 +1,18 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Dispatcher } from '../../types/store';
+import { PointResponse, PointsState } from '../../types/Points';
+import { fetchPoints } from '../../store/Points/thunks';
+
+export const usePoints = (cityId: string | null, page: number): [PointResponse, boolean, string | null] => {
+  const dispatch = useDispatch<Dispatcher>();
+  const { points, error, loading } = useSelector<{
+    points: PointsState;
+  }, PointsState>((state) => state.points);
+
+  useEffect(() => {
+    dispatch(fetchPoints(cityId, page));
+  }, [cityId, page]);
+
+  return [points, loading, error];
+};
