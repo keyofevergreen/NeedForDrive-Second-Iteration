e2f3859d23bd6093db482719c0248894494c6277
evergreen <dany2zverev@gmail.com>
2022-04-27 17:02:40 +0300

    F-10 refactoring

--- src/pages/Rates/Rates.tsx
+++ src/pages/Rates/Rates.tsx
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
-import { ButtonGroup, Form, Table } from 'react-bootstrap';
+import { Table } from 'react-bootstrap';
 import { Helmet } from 'react-helmet';
+import { useRates } from './hooks';
 import useResize from '../../hooks/useResize';
 import ContentContainer from '../../components/ContentContainer/ContentContainer';
 import TableSorting from '../../components/TableSorting/TableSorting';
 import TableItem from '../../components/TableItem/TableItem';
+import FilterByPriceRange from '../../components/FilterByPriceRange/FilterByPriceRange';
 import Spin from '../../components/Spin/Spin';
-import { useRates } from './hooks';
 
 const Rates = (): React.ReactElement => {
   const isResponsive = useResize(1, 1024);
@@ -41,51 +42,12 @@ const Rates = (): React.ReactElement => {
           onResetFilter={resetFilter}
           isFiltered={filters !== null && !(lowerPriceFilter === 0 && higherPriceFilter === 999999)}
         >
-          <ButtonGroup>
-            <Form.Select
-              size="sm"
-              className="range-select"
-              onChange={(e) => setLowerPriceFilter(Number(e.target.value))}
-              value={lowerPriceFilter}
-            >
-              <option value="0">От 0 ₽</option>
-              {higherPriceFilter > 1000 && (
-                <option value="1000">От 1000 ₽</option>
-              )}
-              {higherPriceFilter > 3000 && (
-                <option value="3000">От 3000 ₽</option>
-              )}
-              {higherPriceFilter > 5000 && (
-                <option value="5000">От 5000 ₽</option>
-              )}
-              {higherPriceFilter > 10000 && (
-                <option value="10000">От 10000 ₽</option>
-              )}
-            </Form.Select>
-            <Form.Select
-              size="sm"
-              className="range-select"
-              onChange={(e) => setHigherPriceFilter(Number(e.target.value))}
-              value={higherPriceFilter}
-            >
-              <option value="99999999">До ∞</option>
-              {lowerPriceFilter < 5000 && (
-                <option value="5000">До 5000 ₽</option>
-              )}
-              {lowerPriceFilter < 10000 && (
-                <option value="10000">До 10000 ₽</option>
-              )}
-              {lowerPriceFilter < 25000 && (
-                <option value="25000">До 25000 ₽</option>
-              )}
-              {lowerPriceFilter < 55000 && (
-                <option value="55000">До 55000 ₽</option>
-              )}
-              {lowerPriceFilter < 300000 && (
-                <option value="300000">До 300000 ₽</option>
-              )}
-            </Form.Select>
-          </ButtonGroup>
+          <FilterByPriceRange
+            lowerPriceFilter={lowerPriceFilter}
+            setLowerPriceFilter={setLowerPriceFilter}
+            higherPriceFilter={higherPriceFilter}
+            setHigherPriceFilter={setHigherPriceFilter}
+          />
         </TableSorting>
         <div className="table-container">
           {rates && !loading && !error && (
