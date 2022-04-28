171982eafd552583d0f3a98380ed6bfee191ea95
evergreen <dany2zverev@gmail.com>
2022-04-28 11:54:18 +0300

    V-11 refactoring

--- src/store/Points/thunks.ts
+++ src/store/Points/thunks.ts
@@ -5,7 +5,7 @@ import { addToErrorHandler } from '../ErrorProvider/actions';
 export const fetchPoints = (config): ThunkResult => async (dispatch, getState, { services }) => {
   try {
     dispatch(requestPoints());
-    const { data } = await services.table.getEntities('pointdfdf', config);
+    const { data } = await services.table.getEntities('point', config);
     dispatch(requestPointsSuccess(data));
   } catch (error) {
     dispatch(requestPointsError(error.response.status));
